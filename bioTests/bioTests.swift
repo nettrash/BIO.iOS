b4f53a0bef620c642d359a31e7a5f0f50d5aa17d
NETTRASH <nettrash@nettrash.ru>
2018-04-03 10:45:45 +0300

    Initial Commit

---
+++ bioTests/bioTests.swift
@@ -0,0 +1,36 @@
+//
+//  bioTests.swift
+//  bioTests
+//
+//  Created by Иван Алексеев on 03.04.2018.
+//  Copyright © 2018 NETTRASH. All rights reserved.
+//
+
+import XCTest
+@testable import bio
+
+class bioTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
