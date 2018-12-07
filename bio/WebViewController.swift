bc754e0fa13474e5de094ce0291879c448545fce
NETTRASH <nettrash@nettrash.ru>
2018-12-07 12:04:03 +0300

    fix 3ds

--- bio/WebViewController.swift
+++ bio/WebViewController.swift
@@ -33,9 +33,10 @@ class WebViewController : BaseViewController, WKNavigationDelegate {
 	
 	func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
 		let url = webView.url?.absoluteString
-		if url?.starts(with: "https://service.biocoin.pro/PaymentPortal/WS/State") ?? false {
+		if url?.starts(with: "https://service.biocoin.pro/PaymentPortal/WS/State") ?? url?.starts(with: "https://sib.cards/WS/State") ?? false {
 			let c = URLComponents.init(string: url!)
 			(UIApplication.shared.delegate as! AppDelegate).model!.buyOpKey = c?.queryItems?.filter { $0.name == "OpKey" }.first!.value ?? ""
+			print((UIApplication.shared.delegate as! AppDelegate).model!.buyOpKey);
 			performSegue(withIdentifier: unwindIdentifiers["3ds-auth"]!, sender: self)
 		}
 	}
